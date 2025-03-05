b70fab8b7a279b32522bde4109bcd876e46f88ec
emaa95 <chiletti.ema@gmail.com>
2025-03-05 15:54:05 -0300

    Level extra
    The application takes a few seconds to display the products without showing a 'loading...' message - done

--- buscador-de-lista/caso-0/src/App.tsx
+++ buscador-de-lista/caso-0/src/App.tsx
@@ -3,6 +3,7 @@ import type {Product} from "./types";
 import {useEffect, useState} from "react";
 
 import api from "./api";
+import spinner from './circle-fade.svg';
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -17,13 +18,17 @@ function App() {
       <h1>Tienda digitaloncy</h1>
       <input name="text" placeholder="tv" type="text" onChange={(e) => setQuery(e.target.value)} />
       <ul>
-        {products.map((product) => (
+        {products.length > 0 ? products.map((product) => (
           <li key={product.id} className={product.price <= 100 ? 'sale' : ''}>
             <h4>{product.title}</h4>
             <p>{product.description}</p>
             <span>$ {product.price}</span>
           </li>
-        ))}
+        )) 
+        : <div>
+        <span>Loading...</span><img src={spinner} alt="spinner" width={"50px"} height={"50px"}/>
+        </div>
+      }
       </ul>
     </main>
   );
