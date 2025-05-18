f6cc57340ed60de3ac3ad5c941d4811d7257907c
emaa95 <chiletti.ema@gmail.com>
2025-05-18 19:19:50 -0300

    refactor: extract Movies and improve semantic structure in App

--- react-buscador-peliculas/src/App.jsx
+++ react-buscador-peliculas/src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import './App.css'
 import responseMovies from './mocks/with-results.json'
 import withoutResults from './mocks/no-results.json'
+import { Movies } from './components/Movies'
 
 function App() {
   const movies = responseMovies.Search
-  const hasMavies = movies.length > 0
+  
 
   return (
     <div className='page'>
@@ -19,24 +20,7 @@ function App() {
       </header>
 
       <main>
-        {
-          hasMavies ?
-          (
-            <ul>
-              {
-                movies.map( movie => (
-                  <li key={movie.imdbID}>
-                    <h3>{movie.Title}</h3>
-                    <p>{movie.Year}</p>
-                    <img src={movie.Poster} alt={movie.Title}/>
-                  </li>
-                ))
-              }
-            </ul>
-          ) : (
-            <p>No se encontraron películas para esta búsqueda</p>
-          )
-        }
+        <Movies movies={movies}/>
       </main>
 
     </div>
