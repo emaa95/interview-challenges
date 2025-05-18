95598cee2487473d2652ac3765eb84a6ba0f13ba
emaa95 <chiletti.ema@gmail.com>
2025-05-18 16:03:31 -0300

    feat: list movies with title, year, and poster

--- react-buscador-peliculas/src/App.jsx
+++ react-buscador-peliculas/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import './App.css'
+import responseMovies from './mocks/with-results.json'
+import withoutResults from './mocks/no-results.json'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const movies = responseMovies.Search
+  const hasMavies = movies.length > 0
 
   return (
     <div className='page'>
@@ -16,7 +19,24 @@ function App() {
       </header>
 
       <main>
-
+        {
+          hasMavies ?
+          (
+            <ul>
+              {
+                movies.map( movie => (
+                  <li key={movie.imdbID}>
+                    <h3>{movie.Title}</h3>
+                    <p>{movie.Year}</p>
+                    <img src={movie.Poster} alt={movie.Title}/>
+                  </li>
+                ))
+              }
+            </ul>
+          ) : (
+            <p>No se encontraron películas para esta búsqueda</p>
+          )
+        }
       </main>
 
     </div>
