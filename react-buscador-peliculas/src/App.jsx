806f06e8393df15bb5e7724395179b85781fd8af
emaa95 <chiletti.ema@gmail.com>
2025-05-19 21:13:39 -0300

    feat: make search form functional

--- react-buscador-peliculas/src/App.jsx
+++ react-buscador-peliculas/src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
+import { useRef } from 'react'
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 
 function App() {
   const {movies: mappedMovies} = useMovies()
+  const inputRef = useRef()
+
+  const handleSubmit  = (event) => {
+    event.preventDefault()
+    const value = inputRef.current.value
+    console.log(value)
+  }
 
   return (
     <div className='page'>
 
       <header>
         <h1>Prueba Técnica - Buscador de películas</h1>
-       <form className='form'>
-          <input type="text" placeholder='Avengers, Star Wars, Avatar ...' />
+       <form className='form' onSubmit={handleSubmit}>
+          <input ref={inputRef} type="text" placeholder='Avengers, Star Wars, Avatar ...' />
           <button type='submit'>Buscar</button>
         </form>
       </header>
